29e5dead91b9682c414739a49eeb13c63e210ab6
Michael Ng <mng10@mail.sfsu.edu>
2020-09-03 12:49:05 -0700

    Chapter 2.3

--- Github-Michael-Ng/ViewController.swift
+++ Github-Michael-Ng/ViewController.swift
@@ -10,11 +10,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    //add IBOutlet
+    @IBOutlet weak var messageLabel: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    @IBAction func messagePressed(_ sender: UIButton) {
+        messageLabel.text="You are greatness!"
+    }
+    
 
 }
 
